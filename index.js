cb9baa2297f617e1a87ae7fbc4dbeb06f91346d5
AmazyJoe <ochiengolu@gmail.com>
2022-12-16 13:48:05 +0300

    Done

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  
+
+  for(let i = 0; i < array.length-1; i++){
+   
+    for(let j = i+1; j < array.length; j++){
+        
+      if(array[i]+array[j] === target){
+        return true
+      }
+    }
+  } 
+
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
+  
 if (require.main === module) {
-  // add your own custom tests in here
+
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +30,8 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
 
+}
 module.exports = hasTargetSum;
+
+
